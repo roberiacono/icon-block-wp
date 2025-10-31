c4c8d1c61b0f09bb97cdf34186111e79ae37a69e
Roberto <roberiacono@gmail.com>
2025-10-31 10:19:28 +0100

    remove 'Icon' from name if there is not an icon ending with 'Icon' to keep backward compatibility

--- build/icon-block-wp/index.asset.php
+++ build/icon-block-wp/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2f9d00c02c6574b2e54a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0574ba1f3c697719e7c5');
--- src/icon-block-wp/includes/get-icon.js
+++ src/icon-block-wp/includes/get-icon.js
@@ -3,7 +3,14 @@ import { isString } from "lodash";
 import { parseIcon } from "./parse-icon.js";
 
 export function getIcon(name) {
-	const icon = availableIcons[name];
+	let icon = availableIcons[name];
+
+	// to keep backward compatibility with names ending with "Icon" for Lucide Icons.
+	// TODO: remove this in future major release.
+	if (!icon) {
+		name = name.replace("Icon", "");
+		icon = availableIcons[name];
+	}
 
 	if (!icon) return null; // handle missing icons safely
 
